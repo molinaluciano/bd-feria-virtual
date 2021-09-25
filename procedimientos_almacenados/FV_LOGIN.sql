d4b178abae3eb3c10ff4b7658dfaab30b81081e8
Luciano Molina <lucianoma63@gmail.com>
2021-09-25 20:50:42 -0300

    change base procedure

--- procedimientos_almacenados/FV_LOGIN.sql
+++ procedimientos_almacenados/FV_LOGIN.sql
@@ -2,96 +2,80 @@ CREATE OR REPLACE PROCEDURE FV_LOGIN
  (
     CORREO_IN IN  VARCHAR2,
     CONTRASENA_IN IN  VARCHAR2,
-    ID_TIPO_USUARIO_IN IN VARCHAR2,
-    STATUS_OUT OUT NUMBER
-   
+    ID_TIPO_USUARIO_IN IN NUMBER,
+    STATUS_OUT OUT NUMBER, 
+    TIPO_USUARIO_OUT OUT VARCHAR2
 )
 AS
- ADMINISTRADOR BOOLEAN := FALSE;
- PRODUCTOR BOOLEAN := FALSE;
- CLIENTE BOOLEAN := FALSE;
- TRANSPORTISTA BOOLEAN := FALSE;
- CONSULTOR BOOLEAN := FALSE;
  COUNT_USER NUMBER := 0;
- 
+
 BEGIN
- 
-
-
-    IF ID_TIPO_USUARIO_IN NOT BETWEEN 1 AND 4 THEN
+    IF ID_TIPO_USUARIO_IN NOT BETWEEN 1 AND 5  OR CORREO_IN = '' OR CORREO_IN IS NULL OR CONTRASENA_IN = '' OR CONTRASENA_IN IS NULL THEN
         STATUS_OUT:= 0;
-    ELSE
-        
+    ELSE       
         CASE ID_TIPO_USUARIO_IN 
             WHEN 1 THEN 
-                ADMINISTRADOR := TRUE;
                 DBMS_OUTPUT.PUT_LINE('ADMINISTRADOR');
-            WHEN 2 THEN 
-                PRODUCTOR := TRUE;
-                DBMS_OUTPUT.PUT_LINE('PRODUCTOR');       
-            WHEN 3 THEN
-                CLIENTE := TRUE;
+                SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN AND ID_TIPO_USUARIO = 1;
+                    IF  COUNT_USER > 0 THEN
+                        STATUS_OUT:= 1;
+                        TIPO_USUARIO_OUT:= 'ADMINISTRADOR';
+                    ELSE 
+                        STATUS_OUT:= 0;
+                    END IF;
+            WHEN 2 THEN
                 DBMS_OUTPUT.PUT_LINE('CLIENTE');
-            WHEN 4 THEN
-                TRANSPORTISTA := TRUE;
-                DBMS_OUTPUT.PUT_LINE('TRANSPORTISTA');
-            WHEN 5 THEN
-                CONSULTOR := TRUE;
+                SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN AND ID_TIPO_USUARIO != 1 AND ID_TIPO_USUARIO != 4;
+               
+                    IF COUNT_USER > 0 THEN
+                        SELECT  TU.CATEGORIA INTO TIPO_USUARIO_OUT FROM USUARIO U JOIN TIPO_USUARIO TU ON (U.ID_TIPO_USUARIO = TU.ID_TIPO_USUARIO) 
+                        WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN;
+                        
+                        STATUS_OUT:=1;
+                    ELSE 
+                         STATUS_OUT:= 0;
+                    END IF;
+                    
+            WHEN 3 THEN
                 DBMS_OUTPUT.PUT_LINE('CONSULTOR');
+                SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN  AND ID_TIPO_USUARIO = 4;
+              
+                    IF COUNT_USER > 0 THEN
+                        SELECT  TU.CATEGORIA INTO TIPO_USUARIO_OUT FROM USUARIO U JOIN TIPO_USUARIO TU ON (U.ID_TIPO_USUARIO = TU.ID_TIPO_USUARIO) 
+                        WHERE U.CORREO = CORREO_IN AND U.CONTRASENA = CONTRASENA_IN AND U.ID_TIPO_USUARIO = 4;
+                        
+                        STATUS_OUT:=1;
+                    ELSE 
+                        STATUS_OUT:= 0;
+                    END IF;
+                    
+            WHEN 4 THEN 
+                DBMS_OUTPUT.PUT_LINE('PRODUCTOR');
+                SELECT COUNT(*) INTO COUNT_USER FROM PRODUCTOR 
+                WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN;
+                    IF COUNT_USER > 0 THEN
+                        STATUS_OUT:=1;
+                        TIPO_USUARIO_OUT:= 'PRODUCTOR';
+                    ELSE 
+                        STATUS_OUT:= 0;
+                    END IF;
+            WHEN 5 THEN
+                DBMS_OUTPUT.PUT_LINE('TRANSPORTISTA');
+                SELECT COUNT(*) INTO COUNT_USER FROM TRANSPORTISTA 
+                WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN;
+                    IF COUNT_USER > 0 THEN
+                        STATUS_OUT:=1;
+                        TIPO_USUARIO_OUT:= 'TRANSPORTISTA';
+                    ELSE 
+                        STATUS_OUT:= 0;
+                    END IF;
         END CASE;
     END IF;
-
-    IF ADMINISTRADOR = TRUE THEN
-        SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN;
-            IF  COUNT_USER > 0 THEN
-                STATUS_OUT:= 1;
-            END IF;
-    ELSIF PRODUCTOR = TRUE THEN
-        SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN ;
-            IF COUNT_USER > 0 THEN
-                STATUS_OUT:=1;
-            END IF;
-    ELSIF CLIENTE = TRUE THEN
-        SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN ;
-            IF COUNT_USER > 0 THEN
-                STATUS_OUT:=1;
-            END IF;
-    ELSIF TRANSPORTISTA = TRUE THEN
-            SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN ;
-                IF COUNT_USER > 0 THEN
-                    STATUS_OUT:=1;
-                    NULL;
-                END IF;
-    ELSIF CONSULTOR = TRUE THEN
-        SELECT COUNT(*) INTO COUNT_USER FROM USUARIO WHERE CORREO = CORREO_IN AND CONTRASENA = CONTRASENA_IN ;
-            IF COUNT_USER > 0 THEN
-                STATUS_OUT:=1;
-                NULL;
-            END IF;
-    END IF;
-
-DBMS_OUTPUT.PUT_LINE('FG');
-
-DBMS_OUTPUT.PUT_LINE('CORREO:' || CORREO_IN);
-DBMS_OUTPUT.PUT_LINE('CONTRASENA_IN:' || CONTRASENA_IN);
-DBMS_OUTPUT.PUT_LINE('ID_TIPO_USUARIO_IN:' || ID_TIPO_USUARIO_IN);
-DBMS_OUTPUT.PUT_LINE('STATUS_OUT:' || STATUS_OUT);
 
 
 
 EXCEPTION
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE('UP HUBO UN ERROR' );
-   
-END;
-
-SET SERVEROUTPUT ON;
-
-
-DECLARE 
-     STATUS_OUT NUMBER;
-BEGIN
-    FV_LOGIN('luciano@gmaiil.com','aaa',1 , STATUS_OUT);
-    DBMS_OUTPUT.PUT_LINE('STATUS_OUT WHEN EXECUTE:' ||STATUS_OUT);
-    
+        STATUS_OUT:= 0;
+        DBMS_OUTPUT.PUT_LINE('UPS HUBO UN ERROR' );
 END;
